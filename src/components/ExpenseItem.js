1cdc3a292e7907fc903a03f1d4897d2483e7aa77
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-27 02:11:09 +0600

    make dynamic expenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 10, 27);
+  const expenseTitle = "Car insurance";
+  const expenseAmount = 2994.67;
+
   return (
     <div className="expense-item">
-      <div>October 26th 2022</div>
+      <h1>Heading</h1>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
