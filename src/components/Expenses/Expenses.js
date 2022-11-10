3d5252bf51194d6d168a6c1e7315b3ef7826bccd
Asif-newaz <asif.newaz0093@gmail.com>
2022-11-10 21:15:35 +0600

    runs map through the expenses array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,16 @@ import "./Expenses.css";
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
+      {props.items.map((expenses) => {
+        return (
+          <ExpenseItem
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        );
+      })}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -24,7 +33,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 };
