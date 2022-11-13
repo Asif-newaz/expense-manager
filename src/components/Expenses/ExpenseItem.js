ae7386e599b504219f317590aa3fc4e1be48c6ec
Asif-newaz <asif.newaz0093@gmail.com>
2022-11-14 02:38:11 +0600

    add functionality for filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // const [title, setTitle] = useState(props.title);
-
-  // console.log("ExpenseItems evaluated by react!");
-
-  // const clickHandler = () => {
-  //   setTitle("Updated!");
-  //   console.log(title);
-  // };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -21,9 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button className="button">
-        Change the title
-      </button>
+      <button className="button">Change the title</button>
     </Card>
   );
 };
