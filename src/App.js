a8f4a8e0aedaa8e26830be236770c9e45f700235
Asif-newaz <asif.newaz0093@gmail.com>
2022-11-25 00:52:17 +0600

    add background color to the parent container

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div style={{backgroundColor: "#000"}}>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
