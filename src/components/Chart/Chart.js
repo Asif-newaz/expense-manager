b2391905d83e93a82a704c144fc3950393bf909b
Asif-newaz <asif.newaz0093@gmail.com>
2022-12-31 23:39:46 +0600

    update chart bar file

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Chart = () => {};
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Chart;
