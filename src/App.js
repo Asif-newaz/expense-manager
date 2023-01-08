5ed38be8f0f7bf8cd4fcac86023f566a71de5d8a
Asif-newaz <asif.newaz0093@gmail.com>
2023-01-09 03:06:11 +0600

    update background

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
   };
 
   return (
-    <div style={{backgroundColor: "#000"}}>
+    <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
